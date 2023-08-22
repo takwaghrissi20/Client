aea9baeab76389f831e94103e3f0e58deeca3bb4
takwa <takwaghrissi28@gmail.com>
2023-08-22 09:40:19 +0100

    feat : add routes path in `app.js` file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import ListTodos from "./components/ListTodos";
+import Login from "./components/Login";
+import EditTodo from "./components/EditTodo";
+import InputTodo from "./components/InputTodo";
+import Register from "./components/Register";
+import Forgotpwd from "./components/Forgotpwd";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  //const [user_id,setUser_id]=React.useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/listTodo" element={<ListTodos />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotpwd" element={<Forgotpwd />} />
+      </Routes>
+    </>
   );
 }
 
